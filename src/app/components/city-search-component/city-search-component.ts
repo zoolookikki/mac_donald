b983f8b0524db5bcac66502e3298315f889cb321
Laurent Cordier <laurent.cordier28@orange.fr>
2026-04-29 11:55:26 +0200

    Ajout l’interface de recherche de ville sur la carte (voir details)
    - installation/configuration de TailwindCSS.
    - remplacement des boutons de test par un formulaire de recherche
      réactif.
    - ajout du bouton de recherche avec icône SVG.
    - positionnement responsive de la recherche au-dessus de la carte.
    - ajout d’un overlay bas de page.

--- src/app/components/city-search-component/city-search-component.ts
+++ src/app/components/city-search-component/city-search-component.ts
@@ -1,16 +1,41 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { City } from '../../models/city';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+type CitySearchForm = FormGroup<{
+  city: FormControl<string>;
+}>;
 
 @Component({
   selector: 'app-city-search-component',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './city-search-component.html',
   styleUrl: './city-search-component.css',
 })
 export class CitySearchComponent {
   @Output() currentCity = new EventEmitter<City>();
 
-  public choix1() : void {
+  /* 
+  Typage meilleur que public citySearchForm: FormGroup; ==> voir type CitySearchForm plus haut.
+  C'est un formulaire mais en plus Typescript sait que le champ city de type string.
+  Me permet en plus d'écrire citySearchForm.controls.city.value au lieu de citySearchForm.controls['city'].value + autocompletion.
+  */
+  public citySearchForm: CitySearchForm;
+
+  constructor() {
+    this.citySearchForm = new FormGroup({
+      city: new FormControl('', {
+        /*
+        Typage plus strict permettant d'être sûr que la valeur ne sera jamais nulle. 
+        Evite des tests.
+        */
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+    });
+  }
+
+  private choix1() : void {
     const lieusaint: City = {
       name: 'Lieusaint',
       lat : 48.633331,
@@ -19,7 +44,7 @@ export class CitySearchComponent {
 
     this.currentCity.emit(lieusaint);
   }
-  public choix2() : void {
+  private choix2() : void {
     const sete: City = {
       name: 'Sète',
       lat: 43.400002,
@@ -28,4 +53,42 @@ export class CitySearchComponent {
 
     this.currentCity.emit(sete);
   }
+
+  public onSubmit() {
+    if (this.citySearchForm.invalid) {
+      /*
+      Marque tous les champs du formulaire comme “touchés” => sans markAllAsTouched(), si l’utilisateur clique directement sur le 
+      bouton de validation sans toucher au champ, le message d’erreur pourrait ne pas s’afficher.
+      */
+      this.citySearchForm.markAllAsTouched();
+      return;
+    }
+
+    /*
+    const city = this.citySearchForm.get('city')?.value.trim().toLowerCase();
+    Je récupère directement le contrôle "city" dans la liste des contrôles du formulaire => mieux que le get (si le champs 
+      n'existe pas)
+    */
+    const city: string = this.citySearchForm.controls.city.value.trim().toLowerCase();
+
+    // par protection.
+    if (!city) {
+      this.citySearchForm.markAllAsTouched();
+      return;
+    }    
+
+    if (city === 'lieusaint') {
+      this.choix1();
+      return;
+    }
+
+    if (city === 'sète' || city === 'sete') {
+      this.choix2();
+      return;
+    }
+
+    // Simulation provisoire tant que Nominatim n'est pas branché.
+    this.choix1();
+  }
+
 }
--- src/app/pages/main-page-component/main-page-component.ts
+++ src/app/pages/main-page-component/main-page-component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RestaurantMapComponent } from "../../components/restaurant-map-component/restaurant-map-component";
 import { CitySearchComponent } from "../../components/city-search-component/city-search-component";
 import { City } from '../../models/city';
+import { RestaurantOverlayComponent } from "../../components/restaurant-overlay-component/restaurant-overlay-component";
 
 @Component({
   selector: 'app-main-page-component',
-  imports: [RestaurantMapComponent, CitySearchComponent],
+  imports: [RestaurantMapComponent, CitySearchComponent, RestaurantOverlayComponent],
   templateUrl: './main-page-component.html',
   styleUrl: './main-page-component.css',
 })
